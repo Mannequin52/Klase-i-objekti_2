d086c11324358984679f886af390c1ca8a13a62c
Mannequin52 <92926451+Mannequin52@users.noreply.github.com>
2022-10-19 09:03:45 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Klase_i_objekti_2
+{
+    class KlasaC
+    {
+        public int zbroj(int[] niz);
+
+
+
+
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KlasaC klasaC = new KlasaC();
+            int[] brojevi = new int[5];
+            Console.WriteLine("Upiši 5 cijelih brojeva: ");
+            for (int i = 0; i < 5; i++)
+            {
+                brojevi[i] = Convert.ToInt32(Console.Readline());
+                
+            }
+
+        }
+
+    }
+    
+}
